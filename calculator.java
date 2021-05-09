73bb396a9e93dcaeca71e39843873032c8528941
sasmita2022 <83894371+sasmita2022@users.noreply.github.com>
2021-05-09 16:02:27 +0530

    Commented out

--- calculator.java
+++ calculator.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
            System.out.print("1. Addition\n");
            System.out.print("2. Subtraction\n");
            System.out.print("3. Multiplication\n");
-           System.out.print("4. Division\n");
+           /*System.out.print("4. Division\n");*/
            System.out.print("5. Exit\n\n");
            System.out.print("Enter Your Choice : ");
            choice = scan.next().charAt(0);
